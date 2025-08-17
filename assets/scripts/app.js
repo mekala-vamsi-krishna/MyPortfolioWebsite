3b30e4aca5ba0dea7fb273485f788dad9926dcd1
 Mekala Vamsi Krishna <vamsikrishnayadav2002@gmail.com>
2025-08-18 02:07:58 +0530

    Redesigned complete Portfolio
    Redesigned complete Portfolio

--- assets/scripts/app.js
+++
@@ -1,8 +0,0 @@
-
-function downloadResume() {
-  // Get the anchor tag element
-  var downloadLink = document.getElementById('resume-download-link');
-
-  // Simulate a click on the anchor tag to trigger the download
-  downloadLink.click();
-}
